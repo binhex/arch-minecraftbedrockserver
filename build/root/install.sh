ab877b71c0123864fbb7650b51c7d326171bb9a9
binhex <megalith01@gmail.com>
2020-03-27 15:33:01 +0000

    doh forgot to swtich package to screen

--- build/root/install.sh
+++ build/root/install.sh
@@ -19,7 +19,7 @@ mv /tmp/scripts-master/shell/arch/docker/*.sh /usr/local/bin/
 ####
 
 # define pacman packages
-pacman_packages="rsync tmux"
+pacman_packages="rsync screen"
 
 # install compiled packages using pacman
 if [[ ! -z "${pacman_packages}" ]]; then
