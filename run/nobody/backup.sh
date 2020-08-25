5544b0db05aada3273314b70857dcd597704112e
binhex <megalith01@gmail.com>
2020-08-25 13:04:37 +0100

    bring into line with minecraft java backup

--- run/nobody/backup.sh
+++ run/nobody/backup.sh
@@ -2,14 +2,39 @@
 
 # script to do a manual backup of minecraft worlds in a safe manner, using the save commands via screen
 
+function run_console_command() {
+
+	screen_message="${1}"
+	console_command="${2}"
+
+	retry_count=6
+
+	while ! tail -n 5 '/config/minecraft/logs/screen.log' | grep -q -P "${screen_message}"; do
+
+		screen -r minecraft -p 0 -X stuff "${console_command}^M"
+		retry_count=$((retry_count-1))
+
+		if [ "${retry_count}" -eq "0" ]; then
+
+			echo "[warn] Minecraft console did not confirm message '${screen_message}', giving up waiting..."
+			return 1
+
+		fi
+
+		echo "[info] Waiting for Minecraft console message '${screen_message}', ${retry_count} retries left..."
+		sleep 10s
+
+	done
+	return 0
+
+}
+
 if [[ "${CREATE_BACKUP_HOURS}" -gt 0 ]]; then
 
 	# create backup sub folder to store backups of worlds
 	mkdir -p /config/minecraft/backups
 
 	while true; do
-
-		retry_count=10
 
 		echo "[info] Waiting ${CREATE_BACKUP_HOURS} hours before running worlds backup..."
 		sleep "${CREATE_BACKUP_HOURS}"h
@@ -20,37 +45,31 @@ if [[ "${CREATE_BACKUP_HOURS}" -gt 0 ]]; then
 
 		echo "[info] Starting Minecraft worlds backup..."
 
+		# removing any currently attached sessions (such as web ui console) as you cannot execute 'stuff' whilst another session is connected
+		screen -D
+
+		# removing any dead sessions
+		screen -wipe
+
 		echo "[info] Run Minecraft console command to set Minecraft worlds ready for backup..."
-		screen -r minecraft -p 0 -X stuff "save hold^M"
 
-		while ! tail -n 5 '/config/minecraft/logs/screen.log' | grep -q 'Data saved. Files are now ready to be copied.'; do
+		run_console_command 'Saving|The command is already running' 'save hold'
+		run_console_command 'Files are now ready to be copied' 'save query'
 
-			screen -r minecraft -p 0 -X stuff "save query^M"
-			retry_count=$((retry_count-1))
-
-			if [ "${retry_count}" -eq "0" ]; then
-
-				echo "[warn] Unable to obtain Minecraft worlds in hold state, giving up waiting..."
-				break
-
-			fi
-
-			echo "[info] Waiting for Minecraft databases to be ready for backup, ${retry_count} retries left..."
-			sleep 6s
-
-		done
-
-		if [ "${retry_count}" -gt "0" ]; then
+		if [ "${?}" -eq 0 ]; then
 
 			echo "[info] Minecraft worlds are now ready for backup, backing up to '/config/minecraft/backups/$(date +%Y%m%d-%H%M%S)/'..."
 			cp -R "/config/minecraft/worlds" "/config/minecraft/backups/$(date +%Y%m%d-%H%M%S)"
 
 		fi
 
-		echo "[info] Setting Minecraft back to resume to allow any deferred writes..."
-		screen -r minecraft -p 0 -X stuff "save resume^M"
+		echo "[info] Setting Minecraft back to 'save resume..."
+		run_console_command 'Changes to the level are resumed' 'save resume'
 
 		echo "[info] Minecraft worlds backup complete"
+
+		# restart webui console as it may of been terminated due to forced detachment (see above)
+		nohup /home/nobody/webui.sh >> '/config/supervisord.log' &
 
 	done
 
