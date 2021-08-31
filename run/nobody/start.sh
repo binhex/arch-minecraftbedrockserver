54a7dc9c99ea628bea70876ce2ee6dd71b801b41
binhex <megalith01@gmail.com>
2021-08-31 11:18:10 +0100

    put copy block into function to mirror mc server

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+function copy_minecraft(){
+
+	# if minecraft server.properties file doesnt exist then copy default to host config volume
+	if [ ! -f "/config/minecraft/server.properties" ]; then
+
+		echo "[info] Minecraft server.properties file doesnt exist, copying default installation to '/config/minecraft/'..."
+
+		mkdir -p /config/minecraft
+		if [[ -d "/srv/minecraft" ]]; then
+			cp -R /srv/minecraft/* /config/minecraft/ 2>/dev/null || true
+		fi
+
+	else
+
+		# rsync options defined as follows:-
+		# -r = recursive copy to destination
+		# -l = copy source symlinks as symlinks on destination
+		# -t = keep source modification times for destination files/folders
+		# -p = keep source permissions for destination files/folders
+		echo "[info] Minecraft folder '/config/minecraft' already exists, rsyncing newer files..."
+		rsync -rltp --exclude 'worlds' --exclude '/server.properties' --exclude '/*.json' --exclude '*.debug' '/srv/minecraft/' '/config/minecraft'
+
+	fi
+
+}
+
 function start_minecraft() {
 
 	# create logs sub folder to store screen output from console
@@ -29,33 +55,8 @@ function startup_cmd() {
 
 }
 
-# if minecraft server.properties file doesnt exist then copy default to host config volume
-if [ ! -f "/config/minecraft/server.properties" ]; then
-
-	echo "[info] Minecraft server.properties file doesnt exist, copying default installation to '/config/minecraft/'..."
-
-	mkdir -p /config/minecraft
-	if [[ -d "/srv/minecraft" ]]; then
-		cp -R /srv/minecraft/* /config/minecraft/ 2>/dev/null || true
-	fi
-
-else
-
-	# rsync options defined as follows:-
-	# -r = recursive copy to destination
-	# -l = copy source symlinks as symlinks on destination
-	# -t = keep source modification times for destination files/folders
-	# -p = keep source permissions for destination files/folders
-	echo "[info] Minecraft folder '/config/minecraft' already exists, rsyncing newer files..."
-	rsync -rltp --exclude 'worlds' --exclude '/server.properties' --exclude '/*.json' --exclude '*.debug' '/srv/minecraft/' '/config/minecraft'
-
-fi
-
-echo "[info] Starting Minecraft Bedrock process in screen session 'minecraft'..."
-echo "[info] To attach to the screen session run:-"
-echo "[info] docker exec -u nobody -it <container name> screen -r minecraft"
-echo "[info] To detach from the screen session press:-"
-echo "[info] CTRL+a and then release keys and press d"
+# copy/rsync minecraft to /config
+copy_minecraft
 
 # start minecraft
 start_minecraft
